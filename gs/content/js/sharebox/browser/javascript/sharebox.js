5ca5ef407999582189910e98f01ec3286231c95c
Richard Waid <richard@iopen.net>
2010-07-22 21:53:58 +1200

    Add digg and twitter, split out public and general widgets

--- gs/content/js/sharebox/browser/javascript/sharebox.js
+++ gs/content/js/sharebox/browser/javascript/sharebox.js
@@ -1,44 +1,71 @@
 // Code to power a very simple slide down widget, for sharing a URL, for example
 // a post.
 
-sharewidget = '<span class="share-links"> \
-                 <strong>Share:</strong> \
-                 <a class="fb-link" title="Share on Facebook" \
-                    onclick="javascript:fb_popup(\'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}\')">&#160;</a> \
-                 <span class="email-link" title="A short link to this post">&#160;</span> \
+fb_widget = '<a class="fb-share" title="Share on Facebook" \
+               onclick="javascript:fb_popup(\'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}\')">&#160;</a>'
+
+twitter_widget = '<a class="twitter-share" title="Share on Twitter" \
+               onclick="javascript:fb_popup(\'http://www.twitter.com/home?status=Check+out+this+link:+{HREF}\')">&#160;</a>'
+
+digg_widget = '<a class="digg-share" title="Share on Digg" \
+               onclick="javascript:fb_popup(\'http://digg.com/submit?url={HREF}&title={TITLE}\')">&#160;</a>'
+
+email_widget = '<span class="email-share" title="A short link to this post">&#160;</span> \
                </span> \
-               <div class="full-link"> \
-                 <p class="full-link-box-help">You may copy and paste the link below into an email to link to this post:</p> \
-                 <input class="full-link-input" type="text" value="{HREF}" readonly="0"/> \
+               <div class="full-share"> \
+                 <p class="full-share-box-help">You may copy and paste the link below into an email to link to this post:</p> \
+                 <input class="full-share-input" type="text" value="{HREF}" readonly="0"/> \
                </div>';
+
+share_widget = '<span class="share-widgets"> \
+                 <strong>Share:</strong> \
+                 {WIDGETS}'
+
+publicWidgets = [fb_widget, twitter_widget, digg_widget];
+generalWidgets = [email_widget];
 
 jQuery.noConflict();
 
-jQuery(document).ready ( function() {
-  jQuery('.share-bar').each(function() {
-    var href = jQuery(this).attr('href')
-    var title = jQuery(this).attr('title')
-    sharewidget = sharewidget.replace(/{HREF}/g, href);
-    sharewidget = sharewidget.replace(/{TITLE}/g, title);
-    jQuery(this).html(sharewidget);
-  });
-  jQuery('.email-link').each(function() {
-    jQuery(this).click(function() {
-      fullLink = jQuery(this).parents('.share-bar').find('.full-link');
-      if (fullLink.is(":hidden")) {
-        fullLink.slideDown("slow");
-      } else {
-        fullLink.slideUp("slow");
-      }
+jQuery(document).ready(function(){
+    jQuery('.share-bar').each(function(){
+        var localwidget = share_widget;
+        var href = jQuery(this).attr('href');
+        var title = jQuery(this).attr('title');
+        var isPublic = Boolean(Number(jQuery(this).attr('public')));
+        var allowablewidgets = '';
+		
+        if (isPublic) {
+            for (pos in publicWidgets) {
+                allowablewidgets = allowablewidgets + publicWidgets[pos];
+            }
+        }
+        
+        for (pos in generalWidgets) {
+            allowablewidgets = allowablewidgets + generalWidgets[pos];
+        }
+        
+        localwidget = localwidget.replace(/{WIDGETS}/g, allowablewidgets);
+        localwidget = localwidget.replace(/{HREF}/g, href);
+        localwidget = localwidget.replace(/{TITLE}/g, title);
+        jQuery(this).html(localwidget);
     });
-  });
+    jQuery('.email-share').each(function(){
+        jQuery(this).click(function(){
+            fullLink = jQuery(this).parents('.share-bar').find('.full-share');
+            if (fullLink.is(":hidden")) {
+                fullLink.slideDown("slow");
+            }
+            else {
+                fullLink.slideUp("slow");
+            }
+        });
+    });
 });
 
-
-function fb_popup(url) {
-	newwindow=window.open(url,'name','height=200,width=550');
-	if (window.focus) {newwindow.focus()}
-	return false;
+function fb_popup(url){
+    newwindow = window.open(url, 'name', 'height=200,width=550');
+    if (window.focus) {
+        newwindow.focus()
+    }
+    return false;
 }
-
-
