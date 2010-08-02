f4bb64315da6399fd5727d239e20404e2d9ae10b
Michael JasonSmith <mpj17@onlinegroups.net>
2010-08-02 15:08:42 +1000

    Changing version.py to a version that works.

--- version.py
+++ version.py
@@ -1,14 +1,16 @@
 version='1.0'
 release=False
 
-import commands, os
+import commands, glob, os
 def get_version():
     version_string = ''
     if not release:
         status,output = commands.getstatusoutput("hg tip --template '{node|short}'")
         if status != 0:
-            if os.path.exists('gs.image.egg-info/PKG-INFO'):
-                for line in file('gs.image.egg-info/PKG-INFO'):
+            pkginfo = os.path.join(glob.glob('*.egg-info')[0],
+                                   'PKG-INFO')
+            if os.path.exists(pkginfo):
+                for line in file(pkginfo):
                     if line.find('Version: ') == 0:
                         version_string = line.strip().split('Version: ')[1].strip()
             if not version_string:
@@ -17,5 +19,4 @@ def get_version():
             version_string += '%s-%s' % (version, output)
     
     return version_string
-       
-    
+
