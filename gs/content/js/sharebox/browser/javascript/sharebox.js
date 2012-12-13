2c5dc5533ab8c787794d0d244c33d5721eddead6
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-13 15:48:06 +1100

    Changing the default sharebox code.
    --HG--
    rename : gs/content/js/sharebox/browser/javascript/sharebox-2.js => gs/content/js/sharebox/browser/javascript/sharebox.js

--- gs/content/js/sharebox/browser/javascript/sharebox.js
+++ gs/content/js/sharebox/browser/javascript/sharebox.js
@@ -1,74 +1,169 @@
-// Code to power a very simple slide down widget, for sharing a URL, for example
-// a post.
-
-fb_widget = '<a class="fb-share" title="Share on Facebook" \
-               onclick="javascript:fb_popup(\'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}\')">&#160;</a>'
-
-twitter_widget = '<a class="twitter-share" title="Share on Twitter" \
-               onclick="javascript:fb_popup(\'http://www.twitter.com/home?status={TITLE}:+{HREF}\')">&#160;</a>'
-
-digg_widget = '<a class="digg-share" title="Share on Digg" \
-               onclick="javascript:fb_popup(\'http://digg.com/submit?url={HREF}&title={TITLE}\')">&#160;</a>'
-
-email_widget = '<span class="email-share" title="Emailable link">&#160;</span> \
-               </span> \
-               <div class="full-share"> \
-                 <p class="full-share-box-help">Link to this by copy/pasting the URL below into an email:</p> \
-                 <input class="full-share-input" type="text" value="{HREF}" readonly="0"/> \
-               </div>';
-
-//  TODO: Alllow the calling code to say what is being shared. For 
-//        example "Share post", "Share topic", or "Share image".
-share_widget = '<span class="share-widgets"> \
-                 <strong>Share:</strong> \
-                 {WIDGETS}'
-
-publicWidgets = [fb_widget, twitter_widget, digg_widget];
-generalWidgets = [email_widget];
-
+// Sharing a link from GroupServer
 jQuery.noConflict();
 
-jQuery(document).ready(function(){
-    jQuery('.share-bar').each(function(){
-        var localwidget = share_widget;
-        var href = jQuery(this).attr('href');
-        var title = jQuery(this).attr('title');
-        var isPublic = Boolean(Number(jQuery(this).attr('public')));
-        var allowablewidgets = '';
-		
-        if (isPublic) {
-            for (pos in publicWidgets) {
-                allowablewidgets = allowablewidgets + publicWidgets[pos];
-            }
-        }
-        
-        for (pos in generalWidgets) {
-            allowablewidgets = allowablewidgets + generalWidgets[pos];
-        }
-        
-        title = title.replace(/ /g, '+')
-        localwidget = localwidget.replace(/{WIDGETS}/g, allowablewidgets);
-        localwidget = localwidget.replace(/{HREF}/g, href);
-        localwidget = localwidget.replace(/{TITLE}/g, title);
-        jQuery(this).html(localwidget);
-    });
-    jQuery('.email-share').each(function(){
-        jQuery(this).click(function(){
-            fullLink = jQuery(this).parents('.share-bar').find('.full-share');
-            if (fullLink.is(":hidden")) {
-                fullLink.slideDown("slow");
-            }
-            else {
-                fullLink.slideUp("slow");
-            }
-        });
-    });
-});
+var GS_CONTENT_JS_SHAREBOX_DIALOG_CLASS = 'gs-content-js-sharebox-dialog';
 
-function fb_popup(url){
-    newwindow = window.open(url, 'name', 'height=400,width=650');
+
+var gs_content_js_sharebox_popup = function(url) {
+    var dialog = null;
+    var newWindow = null;
+
+    dialog = jQuery(this).parents('.'+GS_CONTENT_JS_SHAREBOX_DIALOG_CLASS)[0];
+    dialog.dialog("close");
+
+    newWindow = window.open(url, 'name', 'height=414,width=666');// 37u x 23u
     if (window.focus) {
-        newwindow.focus()
+        newWindow.focus()
     }
     return false;
-}
+};//gs_content_js_sharebox_popup
+
+
+var GSShareBox = function (link, isPublic) {
+    // Private variables
+    var button = null;
+    var dialog = null;
+    var url = null;
+    var title = null;
+
+    var FB_URL = 'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}';
+    var FB_WIDGET = '<a class="fb-share dialog button" '+
+        'title="Share on Facebook" href="' + FB_URL + '">f</a>';
+
+    var TWITTER_URL = 'http://www.twitter.com/home?status={TITLE}:+{HREF}';
+    var TWITTER_WIDGET = '<a class="twitter-share dialog button" ' +
+        'title="Share on Twitter"' + 'href="' + TWITTER_URL + '">t</a>';
+
+    var DIGG_URL = 'http://digg.com/submit?url={HREF}&title={TITLE}';
+    var DIGG_WIDGET = '<a class="digg-share dialog button" '+
+        'title="Share on Digg" href="' + DIGG_URL + '">D</a>';
+
+    // --=mpj17=-- Yes, the mailbox part of a mailto can be blank
+    // http://www.ietf.org/rfc/rfc2368.txt
+    var EMAIL_URL = 'mailto:?Subject={TITLE}&body={HREF}';
+    var EMAIL_WIDGET = '<a class="email-share button" title="Share by email"'+
+        'href="' + EMAIL_URL + '">e</a>';
+
+    var public_widgets = [FB_WIDGET, TWITTER_WIDGET, DIGG_WIDGET]
+    //, EMAIL_WIDGET];
+
+   var URL_WIDGET = '<div class="full-share">'+
+        '<p class="full-share-box-help">Link to this by copy/pasting the '+
+        'URL below into an email:</p> '+
+        '<input class="full-share-input" type="text" value="{HREF}"'+
+        'readonly="0"/></div>';
+
+    var buttonOptions = {disabled: false, 
+                         icons: {primary: 'ui-icon-arrowreturnthick-1-w', 
+                                 secondary: null},
+                         label: null, text: true, };
+    var dialogOptions = {autoOpen: false, closeOnEscape: true, 
+                         draggable: false, modal: false, resizable: true};
+
+    // Private methods
+
+
+    var popup_dialog = function(event, data) {
+        var offset = null;
+        var position = null;
+
+        if ( dialog == null ) {
+            dialog = button.after(dialog_html).next();
+            dialog.dialog(dialogOptions);
+            dialog.dialog("option", "title", 'Share ' + title);
+
+            offset = button.offset()
+            position = [offset.left + button.outerWidth(), 
+                        offset.top + button.outerHeight()];
+            dialog.dialog("option", "position", position);
+
+            create_buttons();
+        }
+        dialog.dialog("open");
+    };//popup_dialog
+
+
+    var dialog_html = function () {
+        var retval = null;
+        var privateWidgets = [URL_WIDGET];
+        var i = 0;
+        var privateWidgetString = '';
+        var publicWidgetString = '';
+        var widgetString = '';
+
+        if (isPublic) {
+            for (i in public_widgets) {
+                publicWidgetString = publicWidgetString + public_widgets[i];
+            }
+        }
+        for (i in privateWidgets) {
+            privateWidgetString = privateWidgetString + privateWidgets[i];
+        }
+
+        widgetString = '<div class="gs-content-js-sharebox-dialog-widgets">' +
+            '<div class="gs-content-js-sharebox-dialog-widgets-public">' + 
+            publicWidgetString + '</div>' +
+            '<div class="gs-content-js-sharebox-dialog-widgets-private">' + 
+            privateWidgetString + '</div>'
+        widgetString = widgetString.replace(/{HREF}/g, url);
+        widgetString = widgetString.replace(/{TITLE}/g, title);
+
+        retval = '<div class="' + GS_CONTENT_JS_SHAREBOX_DIALOG_CLASS +
+            '" style="display:none;">' + widgetString + '</div>'
+        return retval;
+    };//dialog_html
+
+
+    var create_buttons = function() {
+        var buttons = null;
+        var button = null;
+        var i = 0;
+        var buttonUrl = null;
+
+        dialog.find('a.button').each(function(){jQuery(this).button()});
+        dialog.find('a.dialog').each(function(){
+            button = jQuery(this);
+            buttonUrl = button.attr('href');
+            button.removeAttr('href');
+            button.data('url', buttonUrl);
+            button.click(dialog_share);
+        });
+    };//create_buttons
+
+
+    var dialog_share = function(event) {
+        var shareButton = null;
+        var shareUrl = null;
+        var newWindow = null;
+
+        shareButton = jQuery(this);
+        shareUrl = shareButton.data('url');
+        dialog.dialog("close");
+
+        // 37u x 23u
+        newWindow = window.open(shareUrl, 'gs-content-js-sharebox-window',
+                                'height=414,width=666');
+        if (window.focus) {
+            newWindow.focus()
+        }
+        return false;
+    };
+
+
+    var setup = function () {
+        button = jQuery(link);
+        url = button.attr('href');
+        button.removeAttr('href');
+        title = button.attr('title');
+    }();//setup **Note** the () is deliberate so this function is run.
+
+    // Public
+
+    return {
+        publicWidgets: public_widgets,
+        init: function () {
+            button.button(buttonOptions);
+            button.click(popup_dialog);
+        },// init
+    }; // Public
+};//GSShareBox
