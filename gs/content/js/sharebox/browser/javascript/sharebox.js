2e66c948cc0367e31886dcbd7db850de9c27115c
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-14 15:05:29 +1100

    Cleaning up the code

--- gs/content/js/sharebox/browser/javascript/sharebox.js
+++ gs/content/js/sharebox/browser/javascript/sharebox.js
@@ -4,52 +4,40 @@ jQuery.noConflict();
 var GS_CONTENT_JS_SHAREBOX_DIALOG_CLASS = 'gs-content-js-sharebox-dialog';
 
 
-var GSShareBox = function (link, isPublic) {
+function GSShareBox(link, isPublic) {
     // Private variables
-    var button = null;
-    var url = null;
-    var title = null;
-
-    var FB_URL = 'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}';
-    var FB_WIDGET = '<a class="fb-share dialog btn" '+
-        'title="Share on Facebook" href="' + FB_URL + '">f</a>';
-
-    var TWITTER_URL = 'http://www.twitter.com/home?status={TITLE}:+{HREF}';
-    var TWITTER_WIDGET = '<a class="twitter-share dialog btn" ' +
-        'title="Share on Twitter"' + 'href="' + TWITTER_URL + '">t</a>';
-
-    var DIGG_URL = 'http://digg.com/submit?url={HREF}&title={TITLE}';
-    var DIGG_WIDGET = '<a class="digg-share dialog btn" '+
-        'title="Share on Digg" href="' + DIGG_URL + '">D</a>';
-    
-    var GP_URL = 'https://plus.google.com/share?url={HREF}';
-    var GP_WIDGET = '<a class="gp-share dialog btn" '+
-        'title="Share on Google+" href="' + GP_URL + '">G+</a>';
-
+    var button = null, url = null, title = null, public_widgets = null,
+        FB_URL = 'http://www.facebook.com/sharer.php?u={HREF}&t={TITLE}',
+        FB_WIDGET = '<a class="fb-share dialog btn" '+
+            'title="Share on Facebook" href="' + FB_URL + '">f</a>',
+        TWITTER_URL = 'http://www.twitter.com/home?status={TITLE}:+{HREF}',
+        TWITTER_WIDGET = '<a class="twitter-share dialog btn" ' +
+            'title="Share on Twitter"' + 'href="' + TWITTER_URL + '">t</a>',
+         DIGG_URL = 'http://digg.com/submit?url={HREF}&title={TITLE}',
+         DIGG_WIDGET = '<a class="digg-share dialog btn" '+
+             'title="Share on Digg" href="' + DIGG_URL + '">D</a>',
+         GP_URL = 'https://plus.google.com/share?url={HREF}',
+         GP_WIDGET = '<a class="gp-share dialog btn" '+
+             'title="Share on Google+" href="' + GP_URL + '">G+</a>',
+         URL_WIDGET = '<div class="full-share">'+
+             '<p class="full-share-help">Link to this by copy/pasting the '+
+             'URL below into an email:</p> <div class="full-share-entry">'+
+             '<input class="full-share-input" type="text" value="{HREF}"'+
+             'readonly="0"/></div></div>';
+    public_widgets = [FB_WIDGET, TWITTER_WIDGET, GP_WIDGET, DIGG_WIDGET];
     // --=mpj17=-- Yes, the mailbox part of a mailto can be blank
     // http://www.ietf.org/rfc/rfc2368.txt
-    var EMAIL_URL = 'mailto:?Subject={TITLE}&body={HREF}';
-    var EMAIL_WIDGET = '<a class="email-share btn" title="Share by email"'+
-        'href="' + EMAIL_URL + '">e</a>';
-
-    var public_widgets = [FB_WIDGET, TWITTER_WIDGET, GP_WIDGET, DIGG_WIDGET]
+    //     EMAIL_URL = 'mailto:?Subject={TITLE}&body={HREF}',
+    //     EMAIL_WIDGET = '<a class="email-share btn" title="Share by email"'+
+    //         'href="' + EMAIL_URL + '">e</a>',
     //, EMAIL_WIDGET];
-
-   var URL_WIDGET = '<div class="full-share">'+
-        '<p class="full-share-help">Link to this by copy/pasting the '+
-        'URL below into an email:</p> <div class="full-share-entry">'+
-        '<input class="full-share-input" type="text" value="{HREF}"'+
-        'readonly="0"/></div></div>';
 
     // Private methods
 
-    var dialog_html = function () {
-        var retval = null;
-        var privateWidgets = [URL_WIDGET];
-        var i = 0;
-        var privateWidgetString = '';
-        var publicWidgetString = '';
-        var widgetString = '';
+    function dialog_html() {
+        var retval = null, privateWidgets = [URL_WIDGET], i = 0, 
+            privateWidgetString = '', publicWidgetString = '',
+            widgetString = '';
 
         if (isPublic) {
             for (i in public_widgets) {
@@ -75,12 +63,8 @@ var GSShareBox = function (link, isPublic) {
     };//dialog_html
 
 
-    var create_buttons = function(event) {
-        var buttons = null;
-        var littleButton = null;
-        var i = 0;
-        var buttonUrl = null;
-        var dialog = null;
+    function create_buttons(event) {
+        var littleButton = null, buttonUrl = null, dialog = null;
 
         dialog = button.next('.popover');
         dialog.find('a.dialog').each(function(){
@@ -93,10 +77,8 @@ var GSShareBox = function (link, isPublic) {
     };//create_buttons
 
 
-    var dialog_share = function(event) {
-        var shareButton = null;
-        var shareUrl = null;
-        var newWindow = null;
+    function dialog_share(event) {
+        var shareButton = null, shareUrl = null, newWindow = null;
 
         shareButton = jQuery(this);
         shareUrl = shareButton.data('url');
@@ -111,9 +93,9 @@ var GSShareBox = function (link, isPublic) {
         return false;
     };
 
-    var setup = function () {
+    function setup() {
         var popoverOptions = {amimation: true, html: true, placement: 'bottom',
-                          trigger: 'click', content: dialog_html}
+                              trigger: 'click', content: dialog_html}
         button = jQuery(link);
         url = button.attr('href');
         button.removeAttr('href');
@@ -121,12 +103,10 @@ var GSShareBox = function (link, isPublic) {
         popoverOptions['title'] = 'Share ' + title;
         button.popover(popoverOptions);
         button.on('click', create_buttons);
-    }();//setup **Note** the () is deliberate so this function is run.
-
+    }//setup
+    setup();//Run on startup
 
     // Public
-
-
     return {
         publicWidgets: public_widgets,
         init: function () {
