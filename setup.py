46c3d6ec2b652948c2dcc8debf8e64df7623a9f5
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-06 15:54:55 -0800

    Not zip_safe.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='gs.content.js.sharebox',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['gs'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           'gs.content.js.bootstrap',
